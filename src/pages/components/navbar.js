9712057a9907cf1b0f1987560dcb2f3a5d0eb8a8
gowthaman-rently <gowthaman.ks@rently.com>
2024-04-28 11:00:08 +0530

    sign in handling

--- src/pages/components/navbar.js
+++ src/pages/components/navbar.js
@@ -1,86 +1,120 @@
 import React from 'react';
-import { AppBar, Container, Toolbar, Typography, MenuItem, Menu, Avatar, IconButton, Tooltip, Box } from '@mui/material';
-import { Link, Navigate } from 'react-router-dom';
-
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  MenuItem,
+  Menu,
+  Avatar,
+  IconButton,
+  Tooltip,
+  Box,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
-    state = {
-        menuStatus : false
-    }
+  state = {
+    menuStatus: false,
+  };
 
-    render() { 
-        console.log(this.props.user);
-        return (
-            <AppBar position="fixed" sx={{backgroundColor:"#282c34"}}>
-                { 
-                    this.props.user === null
-                    ?<Navigate to={"/signin?redirect="+window.location.pathname} />
-                    :""
-                }
-                <Container maxWidth="xl">
-                    <Toolbar sx={{display:"flex",justifyContent:"space-between"}}>
-                        <Box>
-                            <Link to="/" style={{textDecoration:"none"}}>
-                                <Typography
-                                    variant="h5"
-                                    noWrap
-                                    component="div"
-                                    sx={{
-                                        mr: 2,
-                                        flexGrow: 1,
-                                        fontFamily: 'monospace',
-                                        fontWeight: 800,
-                                        letterSpacing: '.1rem',
-                                        color: 'white',
-                                        textDecoration: 'none',
-                                        textAlign: 'start'
-                                    }}
-                                >
-                                    Games World
-                                </Typography>
-                            </Link>
-                        </Box>
-                            
-                        <Box>
-                            <Tooltip title="Profile">
-                                <IconButton onClick={()=>{this.setState({menuStatus : !this.state.menuStatus})}} sx={{ p: 0 }}>
-                                    <Avatar src={this.props.user?.getImageUrl()}></Avatar>
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                id="menu-appbar"
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                anchorEl={this.state.menuStatus}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={this.state.menuStatus}
-                                onClose={()=>{this.setState({menuStatus : !this.state.menuStatus})}}
-                            >
-                                <MenuItem  sx={{width:"100%",justifyContent:"center"}}>
-                                    <Box sx={{width:"100%",textAlign:"center"}}>
-                                        <Avatar src={this.props.user?.getImageUrl()} sx={{margin:"auto"}}></Avatar>
-                                        <Typography variant='h6' component="div">{this.props.user?.getName()}</Typography>
-                                        
-                                        <Typography variant='body2' component="div">{this.props.user?.getEmail()}</Typography>
-                                    </Box>
-                                    <hr/>
-                                </MenuItem>
-                                <MenuItem onClick={()=>{this.setState({menuStatus : !this.state.menuStatus})}} sx={{width:"100%",justifyContent:"center"}}>
-                                    <Typography textAlign="center" onClick={()=>{this.props.setUser(null)}}>Logout</Typography>
-                                </MenuItem>
-                            </Menu>
-                        </Box>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-        );
-    }
+  render() {
+    console.log(this.props.user);
+    return (
+      <AppBar position="fixed" sx={{ backgroundColor: '#282c34' }}>
+        <Container maxWidth="xl">
+          <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Box>
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <Typography
+                  variant="h5"
+                  noWrap
+                  component="div"
+                  sx={{
+                    mr: 2,
+                    flexGrow: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 800,
+                    letterSpacing: '.1rem',
+                    color: 'white',
+                    textDecoration: 'none',
+                    textAlign: 'start',
+                  }}
+                >
+                  Games World
+                </Typography>
+              </Link>
+            </Box>
+            {this.props.user ? (
+              <Box>
+                <Tooltip title="Profile">
+                  <IconButton
+                    onClick={() => {
+                      this.setState({ menuStatus: !this.state.menuStatus });
+                    }}
+                    sx={{ p: 0 }}
+                  >
+                    <Avatar src={this.props.user?.getImageUrl()}></Avatar>
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  id="menu-appbar"
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  anchorEl={this.state.menuStatus}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={this.state.menuStatus}
+                  onClose={() => {
+                    this.setState({ menuStatus: !this.state.menuStatus });
+                  }}
+                >
+                  <MenuItem sx={{ width: '100%', justifyContent: 'center' }}>
+                    <Box sx={{ width: '100%', textAlign: 'center' }}>
+                      <Avatar
+                        src={this.props.user?.getImageUrl()}
+                        sx={{ margin: 'auto' }}
+                      ></Avatar>
+                      <Typography variant="h6" component="div">
+                        {this.props.user?.getName()}
+                      </Typography>
+
+                      <Typography variant="body2" component="div">
+                        {this.props.user?.getEmail()}
+                      </Typography>
+                    </Box>
+                    <hr />
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      this.setState({ menuStatus: !this.state.menuStatus });
+                    }}
+                    sx={{ width: '100%', justifyContent: 'center' }}
+                  >
+                    <Typography
+                      textAlign="center"
+                      onClick={() => {
+                        this.props.setUser(null);
+                      }}
+                    >
+                      Logout
+                    </Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            ) : (
+              <Box><Link to='/signin' style={{fontSize:'18px', fontWeight:'600', textDecoration: 'none',}}>Sign In</Link></Box>
+            )}
+          </Toolbar>
+        </Container>
+      </AppBar>
+    );
+  }
 }
- 
+
 export default NavBar;
